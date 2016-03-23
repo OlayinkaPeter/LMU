737879910bc7f21a282d610484c0c68ad65638e6
OlayinkaPeter <olayinkapeter99@gmail.com>
2016-03-23 12:45:08 +0100

    new changes made to the bla bla

--- js/tab.effect.js
+++ js/tab.effect.js
@@ -21,7 +21,7 @@ function chgTab(name) {
             " in Chaplaincy activities or relax between lectures in the five-star Cafeteria which has an eclectic menu. " +
             "Students can also relax under the manicured trees that are available around or in their hostel common room. " +
             "Generally, life on campus is so structured that it balances academic activities with leisure and spirituality ...";
-        image.src = "images/o.jpg";
+        image.src = "images/p.jpg";
     }
     else if(name == 'apply3'){
         var image = document.getElementById("bg_image");
@@ -31,26 +31,28 @@ function chgTab(name) {
             "The adaption of these concepts to the peculiarity of our indigenous educational context has stabilised academic" +
             " process in the University, engenders a system that afford scholarship and " +
             "scholastic opportunities to staff and student. Landmark University unlike most other universities ...";
-        image.src = "images/p.jpg";
+        image.src = "images/o.jpg";
     }
     else if(name == 'apply4'){
         var image = document.getElementById("bg_image");
         document.getElementById("bg_caption").textContent="News For Parents and Guardians";
-        document.getElementById("bg_note").textContent="Landmark University is fashioned to rank among world class " +
-            "Missionary Universities from where both administrative and academic operational concepts traditionally came. " +
-            "The adaption of these concepts to the peculiarity of our indigenous educational context has stabilised academic" +
-            " process in the University, engenders a system that afford scholarship and " +
-            "scholastic opportunities to staff and student. Landmark University unlike most other universities ...";
+        document.getElementById("bg_note").textContent="Aside academic activities, students enjoy a wide range of other " +
+            "activities like athletics i.e. football, volleyball, table-tennis, lawn tennis. They also participate in Chaplaincy " +
+            "activities or relax between lectures in the five-star Cafeteria which has an eclectic menu. Students can also" +
+            " relax under the manicured trees that are available around or in their hostel common room. Generally, life on " +
+            "campus is so structured that it balances academic activities with leisure and spirituality. ...";
         image.src = "images/q.jpg";
     }
     else if(name == 'apply5'){
         var image = document.getElementById("bg_image");
         document.getElementById("bg_caption").textContent="Research";
-        document.getElementById("bg_note").textContent="Landmark University is fashioned to rank among world class " +
-            "Missionary Universities from where both administrative and academic operational concepts traditionally came. " +
-            "The adaption of these concepts to the peculiarity of our indigenous educational context has stabilised academic" +
-            " process in the University, engenders a system that afford scholarship and " +
-            "scholastic opportunities to staff and student. Landmark University unlike most other universities ...";
+        document.getElementById("bg_note").textContent="Our library is equipped with over 10,000 book collections, " +
+            "which cover wider scope of the University programme. The different " +
+            "sections in the Library are Online Public Access Catalog, " +
+            "Escapist Reading, Serial, Reading, E-Library Cafeteria which has an eclectic menu. Students can also" +
+            " relax under the manicured trees that are available and Spiritual & Leadership Development Section" +
+            "in the Library are Online Public Access Catalog, " +
+            "Escapist Reading, Serial, Reading, E-Library and Spiritual & Leadership Development Section. ...";
         image.src = "images/r.jpg";
     }
     else if(name == 'apply6'){
@@ -62,6 +64,30 @@ function chgTab(name) {
             " process in the University, engenders a system that afford scholarship and " +
             "scholastic opportunities to staff and student. Landmark University unlike most other universities ...";
         image.src = "images/t.jpg";
+    }
+
+    else if(name == 'apply7'){
+        var image = document.getElementById("bg_image");
+        document.getElementById("bg_caption").textContent="Accomodation";
+        document.getElementById("bg_note").textContent="Landmark University, being a full boarding institution ensures that " +
+            "conducive, comfortable and academic activity engendering accommodation provision is made available for all the " +
+            "students; on the University Campus.The University believes that creating a happy and relaxed atmosphere is " +
+            "germane for every student’s successful stay on campus. Currently, there are 6 Residential Halls comprising of 3 " +
+            "Halls each for each sex, and accommodating about 3000 students. Additional structures are presently being " +
+            "...";
+        image.src = "images/accomodation.jpg";
+    }
+
+    else if(name == 'apply8'){
+        var image = document.getElementById("bg_image");
+        document.getElementById("bg_caption").textContent="Alunmi";
+        document.getElementById("bg_note").textContent="Alabi Oluwatobi, 21, graduated with a first class from the Department" +
+            " of Sociology, Landmark University, Omu Aran, Kwara State, in the 2013/2014 academic session with a 4.73 CGPA. He " +
+            "tells TUNDE AJAJA about what attracted him to Sociology ...  It involved extra effort, commitment and sacrificing " +
+            "some pleasures. Definitely, I had to study for extra hours and use the library, but what really gave me a lift " +
+            "academically was my interest in helping others to improve. I took series of tutorial even for borrowed courses " +
+            "....";
+        image.src = "images/Alabi-Oluwatobi.jpg";
     }
 
 }
